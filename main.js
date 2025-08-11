79a1a21c1c8e9d4e1991dbcb002ecacd87add523
mahmoud ellaithy <139589516+mahmoudellaithy2023@users.noreply.github.com>
2025-08-11 16:02:29 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+document.querySelector(".icon").addEventListener("click", () => {
+  document.querySelector("nav ul").classList.toggle("show");
+  document.querySelector(".icon").classList.toggle(".fa-solid fa-xMark");
+});
